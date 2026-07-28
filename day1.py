520031755faad380db556a69204a52eac18c27ac
nithya309 <nithyapothula8341@gmail.com>
2026-07-28 15:15:51 +0530

    Tokens-day1

---
+++ day1.py
@@ -0,0 +1,18 @@
+print('hello codegnan')
+
+'''
+tokens--> a smallest unit in a progrram. there are 5types of tokens..
+keyword
+'''
+name='nithyasree'
+print(name)
+email_id='nithyapothula@8341gmail.com'
+print(email_id)
+
+batch='cse'
+print(batch)
+
+len=5
+bre=3
+area=len*bre
+print(area)
